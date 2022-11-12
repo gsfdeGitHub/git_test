46359d68c7f210cf9521a6e2a53d20a794688247
gsf <gsf@sgu.com>
2022-11-12 21:09:51 +0800

    push test commit

--- src/main/java/com/gsf/GitTest.java
+++ src/main/java/com/gsf/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
             System.out.println("hello3 test merge");
             System.out.println("hello4 test merge2.0");
             System.out.println("hello4 test merge2");
+            System.out.println("push test");
       }
 }
